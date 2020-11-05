dbca0a83c1d6cb40de55c9edcab2cadc9e76dfa1
prodigyoff <maryan.kit@gmail.com>
2020-11-05 16:23:22 +0200

    Minor fixes

--- managers/bananas.py
+++ managers/bananas.py
@@ -4,8 +4,8 @@ from managers.heap_sort import heap_sort
 def find_bananas_per_hour(piles, hours):
     """
     Returns suitable bananas per hour amount
-    >>> find_bananas_per_hour([3, 6, 7, 11], 8)
-    4
+    >>> find_bananas_per_hour([1, 1, 1, 1], 4)
+    1
     >>> find_bananas_per_hour([30, 11, 23, 4, 20], 5)
     30
     >>> find_bananas_per_hour([30, 11, 23, 4, 20], 6)
@@ -13,9 +13,7 @@ def find_bananas_per_hour(piles, hours):
     >>> find_bananas_per_hour([30, 11, 14, 4, 13], 6)
     15
     >>> find_bananas_per_hour([3, 6, 7, 11], 80)
-    Traceback (most recent call last):
-    ...
-    Exception: Can't eat less than 1 banana per hour :(
+    1
     """
     heapified_piles = heap_sort(piles)
     max_value = heapified_piles[0]
@@ -32,7 +30,7 @@ def find_bananas_per_hour(piles, hours):
         if elapsed_time <= hours:
             max_value -= 1
             if max_value < 1:
-                raise Exception("Can't eat less than 1 banana per hour :(")
+                break
         else:
             break
     if max_value == 1:
